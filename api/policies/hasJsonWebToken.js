e97416a1f029fb23d70d6beefb5822ff6e5d5960
daeyoun24 <daeyoun1506@gmail.com>
2016-01-05 22:36:49 +0000

    Enabled Bearer in header for token and changed policies

--- api/policies/hasJsonWebToken.js
+++ api/policies/hasJsonWebToken.js
@@ -10,9 +10,15 @@
  * @docs        :: http://waterlock.ninja/documentation
  */
 module.exports = function(req, res, next) {
-  waterlock.validator.validateTokenRequest(req, function(err, user){
-    if(err){
-      return res.forbidden(err);  
+
+  var authHeader = req.headers['authorization'];
+  if (authHeader.substring(0, 7) !== 'Bearer ') return res.forbidden('Authorization token not included in header');
+  
+  req.headers['access_token'] = authHeader.substring(7);
+
+  waterlock.validator.validateTokenRequest(req, function(err, user) {
+    if (err) {
+      return res.forbidden(err);
     }
 
     // valid request
