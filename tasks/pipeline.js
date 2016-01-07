ecd246733e07a7791f371ffeb044fe330548bb9b
daeyoun24 <daeyoun1506@gmail.com>
2016-01-07 03:48:35 +0000

    Updated

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -1,17 +1,22 @@
 var cssFilesToInject = [
   'bower_components/bootstrap/dist/css/bootstrap.min.css',
   'bower_components/angular-ui-grid/ui-grid.css',
+  'bower_components/angular-xeditable/dist/css/xeditable.css',
   'components/**/*.css'
 ];
 
 var jsFilesToInject = [
   'dependencies/sails.io.js',
-  'bower_components/jquery/dist/jquery.js',
-  'bower_components/angular/angular.js',
-  'bower_components/angular-ui-router/release/angular-ui-router.js',
-  'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-  'bower_components/angular-animate/angular-animate.js',
+  'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/angular/angular.min.js',
+  'bower_components/angular-route/angular-route.min.js',
+  'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+  'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
   'bower_components/angular-ui-grid/ui-grid.js',
+  'bower_components/angular-animate/angular-animate.min.js',
+  'bower_components/moment/min/moment.min.js',
+  'bower_components/angular-moment/angular-moment.min.js',
+  'bower_components/angular-xeditable/dist/js/xeditable.min.js',
   'app.js',
   'components/**/*.js'
 ];
