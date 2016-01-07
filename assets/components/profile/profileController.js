366ada1ab78e460d51ce03d9abea269ed5994d99
daeyoun24 <daeyoun1506@gmail.com>
2016-01-07 07:18:22 +0000

    Updated profile page

--- assets/components/profile/profileController.js
+++ assets/components/profile/profileController.js
@@ -1,5 +1,26 @@
-application.controller('profileController', function($scope, CurrentUser) {
+application.controller('profileController', function($scope, $http, CurrentUser) {
+
+    $scope.user = {
+        id: CurrentUser.user().id,
+        username: CurrentUser.user().username,
+        email: CurrentUser.user().email
+    };
     
-    $scope.username = CurrentUser.user.username;
-    $scope.email = CurrentUser.user.email;
+    $scope.checkEmail = function(data) {
+        // some validation here
+    };
+
+    $scope.updateUser = function() {
+        // $scope.user already updated!
+        return $http.put('/user/update', $scope.user).error(function(err) {
+            if (err.field && err.msg) {
+                // err like {field: "name", msg: "Server-side error for this username!"} 
+                $scope.editableForm.$setError(err.field, err.msg);
+            }
+            else {
+                // unknown error
+                $scope.editableForm.$setError('name', 'Unknown error!');
+            }
+        });
+    };
 });
