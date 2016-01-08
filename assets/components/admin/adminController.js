ba3184729fbc8bdf2bd738b4e34ffb268ef7e318
daeyoun24 <daeyoun1506@gmail.com>
2016-01-08 08:30:29 +0000

    Generated dynamic user table

--- assets/components/admin/adminController.js
+++ assets/components/admin/adminController.js
@@ -1,23 +1,38 @@
-application.controller('adminController', function($scope, CurrentUser) {
-    
-    $scope.user = CurrentUser.user;
-    
+application.controller('adminController', function($scope, $http, CurrentUser) {
+
+    //$scope.user = CurrentUser.user;
+
+    $http.get('/user')
+        .then(function(res) {
+            $scope.gridOptions.data = res.data;
+        })
+        .catch(function(err) {
+            if (err) {
+
+            }
+        });
+
+    $scope.deleteRow = function(row) {
+        var index = $scope.gridOptions.data.indexOf(row.entity);
+        $scope.gridOptions.data.splice(index, 1);
+    };
+
     $scope.gridOptions = {
         enableRowSelection: true,
         enableSorting: true,
         columnDefs: [{
+            name: 'ID',
+            field: 'id'
+        }, {
             name: 'Name',
             field: 'username'
         }, {
             name: 'Email',
             field: 'email'
-        }, ],
-        data: [{
-            username: 'testing',
-            email: 'hello@foo.bar'
         }, {
-            username: 'badguy',
-            email: 'badguy@foo.bar'
+            name: 'Delete',
+            cellTemplate: '<button type="button" class="btn btn-danger btn-xs" ng-click="grid.appScope.deleteRow(row)"><i class="glyphicon glyphicon-remove"></i></button>'
         }]
     };
+
 });
