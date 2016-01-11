fb475874b9568ecd4b47e8b2a8b8afe49bc0c362
Azri Ahmad <azriahmad94@gmail.com>
2016-01-11 01:07:47 -0600

    minor fix in auth.js

--- api/models/Auth.js
+++ api/models/Auth.js
@@ -11,7 +11,7 @@ module.exports = {
   connection: 'MySQLServerMeta',
   autoPK: true,
   migrate: 'alter',
-  tableName: 'Attempt',
+  tableName: 'Auth',
   attributes: require('waterlock').models.auth.attributes({
     
     username: 'string',
