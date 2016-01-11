68683d645cbd24f39fd98287f29c041ca52fe301
daeyoun24 <daeyoun1506@gmail.com>
2016-01-11 06:16:15 +0000

    Minor fixes

--- assets/components/loginModal/loginModalService.js
+++ assets/components/loginModal/loginModalService.js
@@ -1,19 +1,19 @@
-application.service('loginModalService', function( $http, $q, $uibModal, Auth ) {
+application.service('loginModalService', function($http, $q, $uibModal, Auth) {
     return {
-        'open': function () {
-            return $uibModal.open( {
+        'open': function() {
+            return $uibModal.open({
                 animation: true,
                 templateUrl: '/components/loginModal/loginModal.html',
                 controller: 'loginModalController',
                 size: 'sm'
             });
         },
-        'submit': function ( user ) {
+        'submit': function(user) {
             var defer = $q.defer();
-            Auth.login(user).success( function( response ){
-                defer.resolve( response );
-            }).error( function( err ) {
-                defer.reject( err );
+            Auth.login(user).success(function(response) {
+                defer.resolve(response);
+            }).error(function(err) {
+                defer.reject(err);
             });
             return defer.promise;
         }
