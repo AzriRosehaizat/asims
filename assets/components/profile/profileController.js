b8f5ea7815490c98f5ce6536e110e089c4a61fc5
daeyoun24 <daeyoun1506@gmail.com>
2016-01-12 01:32:13 +0000

    Use schemaForm instead of xeditable

--- assets/components/profile/profileController.js
+++ assets/components/profile/profileController.js
@@ -1,43 +1,26 @@
-application.controller('profileController', function($scope, $http, CurrentUser) {
-    
+application.controller('profileController', function($scope, DataService, CurrentUser, UserSchema, UserForm) {
+
     $scope.user = {
         id: CurrentUser.user().id,
         username: CurrentUser.user().username,
         email: CurrentUser.user().email
     };
-    
-    $scope.checkEmail = function(data) {
-        // some validation here...
-    };
-    
-    // keep a copy of user in case of errors
-    $scope.copyUser = function() {
-        $scope.userCopy = angular.copy($scope.user);
-    };
 
-    $scope.updateUser = function() {
-        // $scope.user already updated!
-        return $http.put('/user/update', $scope.user)
-        // .then().catch() doesn't work with xeditable form
-        .success(function(res) {
-            // save the updated user object to local storage
-            CurrentUser.update(res);  
-            $scope.copyUser();
-        }) 
-        .error(function(err) { 
-            if (err.field && err.msg) {
-                // err like {field: "name", msg: "Server-side error for this username!"} 
-                $scope.editableForm.$setError(err.field, err.msg);
-            }
-            else {
-                // unknown error
-                $scope.editableForm.$setError('name', 'Unknown error!');
-            }
-            
-        });
-    };
-    
-    $scope.restoreUser = function() {
-        $scope.user = $scope.userCopy;
+    $scope.schema = UserSchema;
+    $scope.form = UserForm;
+
+    $scope.onSubmit = function(form) {
+        // First we broadcast an event so all fields validate themselves
+        $scope.$broadcast('schemaFormValidate');
+
+        if (form.$valid) {
+            DataService.updateUser($scope.user)
+                .then(function(data) {
+                    console.log(data);
+                    CurrentUser.update(data);
+                }, function(err) {
+                    console.warn(err);
+                });
+        }
     }
 });
