76858d34d2ea60200245621a93b2aae35b4b848c
daeyoun24 <daeyoun1506@gmail.com>
2016-01-13 09:31:04 +0000

    Added a modal for profile view

---
+++ assets/components/profile/profileModalController.js
@@ -0,0 +1,25 @@
+application.controller('profileModalController', function($scope, $uibModalInstance, user, DataService, ProfileSchema, ProfileForm) {
+
+    $scope.user = angular.copy(user);
+    $scope.schema = ProfileSchema;
+    $scope.form = ProfileForm;
+
+    $scope.onSubmit = function(form) {
+        // first we broadcast an event so all fields validate themselves
+        $scope.$broadcast('schemaFormValidate');
+
+        if (form.$valid) {
+            DataService.updateUser($scope.user)
+                .then(function(data) {
+                    user = angular.extend(user, $scope.user);
+                    $uibModalInstance.close(user);
+                }, function(err) {
+                    console.warn(err);
+                });
+        }
+    };
+    
+    $scope.onClose = function() {
+        $uibModalInstance.close(user);
+    }
+});
