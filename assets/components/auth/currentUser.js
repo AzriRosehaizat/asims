4749b883f8365c28b7e9a533a496a18ef7dad5d1
daeyoun24 <daeyoun1506@gmail.com>
2016-01-13 09:34:05 +0000

    Authentication based on roles

--- assets/components/auth/currentUser.js
+++ assets/components/auth/currentUser.js
@@ -1,14 +1,25 @@
 application.factory('CurrentUser', function(LocalService, DataService) {
     return {
         getUser: function() {
-            var id = getID();
-            return DataService.getUser(id)
+            return DataService.getUser(getID())
                 .then(function(data) {
                     return data;
                 });
         },
         getRole: function() {
-            return angular.fromJson(LocalService.get('auth_token')).user.role;
+            var localRole = angular.fromJson(LocalService.get('auth_token')).user.role;
+            this.getUser()
+                .then(function(data) {
+                    if (localRole !== data.role.id) {
+                        console.warn("User's role has been modified! local: " + localRole + ", server: " + data.role.id);
+                        // don't need to do this after we set a policy to handle roles
+                        return LocalService.unset('auth_token');
+                    }
+                    console.log("User's role has been verified. local: " + localRole + ", server: " + data.role.id);
+                }, function(err) {
+                    console.warn(err);
+                });
+            return localRole;
         },
     };
 
@@ -17,6 +28,7 @@ application.factory('CurrentUser', function(LocalService, DataService) {
             return angular.fromJson(LocalService.get('auth_token')).user.id;
         }
         else {
+            console.log("There's something wrong with the token. Please log out and log in again");
             return {};
         }
     }
