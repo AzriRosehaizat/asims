89278f1753f95f0fe872f4d782a382aab69af0f7
Azri Ahmad <azriahmad94@gmail.com>
2016-01-16 21:53:04 -0600

    Initial test

--- config/routes.js
+++ config/routes.js
@@ -35,7 +35,7 @@ module.exports.routes = {
   // '/': {
   //   view: 'homepage'
   // }
-
+  'GET /AcademicStaff/test' : 'AcademicStaffController.test'
   /***************************************************************************
   *                                                                          *
   * Custom routes here...                                                    *
