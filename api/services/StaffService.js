e14c25854496148073c4e3c7f52d3d42f6e577ab
Azri Ahmad <azriahmad94@gmail.com>
2016-01-17 16:32:44 -0600

    abstract staff to parent
    more information later

--- api/services/StaffService.js
+++ api/services/StaffService.js
@@ -11,40 +11,50 @@
 module.export = {
 	/**
 	* Return a list of all RAS ID
-	* @req None
+	* @req:
+	* 		type: type of AcademicStaff
+	* @
 	* @res list of all RAS ID
 	*/
-	getRAS: function(req, res){
-		RegularStaff.find({ select: ['academicStaffID'] ;
-		},function(err, RAS){
+	getStaff: function(req, res){
+		var type = req.param('type').toLowerCase();
+		var selObj = {select: ['academicStaffID']};
+		var cbHell = function (err, staff){
 			if (err) return res.negotiate(err);           
-	  		return res.json(RAS); 
-		});
+	  		return res.json(staff); 
+		};
+		console.log(type);
+		if (type ===  "ras")
+			RegularStaff.find(selObj, cbHell);
+		else if (type === "cas")
+			ContractStaff.find(selObj, cbHell);
+		else
+			return res.json({notFound:true})
 	},
 
 	//Get department given a related staff ID
 	getDepartment: function(req, res){
-	var ID = req.param
-	AcademicStaff.findOne({staffID: ID})
-	.then(function(AcademicStaff){
-		if(AcademicStaff===undefined)
-		return res.json({notFound:true});
-		//Get departments
-		var departments = AcademicStaff_Department.find( { staffID : AcademicStaff.staffID } ).populate('departmentID').then( function(departments){
-                console.log(departments);
-                return departments;
-        });
-		return [AcademicStaff.staffID, AcademicStaff.firstName, AcademicStaff.lastName, departments];
+		var ID = req.param
+		AcademicStaff.findOne({staffID: ID})
+		.then(function(AcademicStaff){
+			if(AcademicStaff===undefined)
+			return res.json({notFound:true});
+			//Get departments
+			var departments = AcademicStaff_Department.find( { staffID : AcademicStaff.staffID } ).populate('departmentID').then( function(departments){
+	                console.log(departments);
+	                return departments;
+	        });
+			return [AcademicStaff.staffID, AcademicStaff.firstName, AcademicStaff.lastName, departments];
 
-	}).spread( function(staffID, firstName, lastName, departments ){
-        res.json({
-        	'staffID': staffID,
-            'firstName': firstName, 
-          	'lastName': lastName,
-            'departments': departments
-        });
-    }).catch(function(err) {
-    	return res.serverError(err);
-    })
-}
+		}).spread( function(staffID, firstName, lastName, departments ){
+	        res.json({
+	        	'staffID': staffID,
+	            'firstName': firstName, 
+	          	'lastName': lastName,
+	            'departments': departments
+	        });
+	    }).catch(function(err) {
+	    	return res.serverError(err);
+	    })
+	}
 };
