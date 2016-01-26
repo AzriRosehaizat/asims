61b2a12ebf0c1645753a4be601dba213574bba58
james10011 <jamesmackay10011@gmail.com>
2016-01-26 22:18:25 +0000

    Implemented population on Department Academic Staff and Regular Staff, fixed efficiency issues

--- api/models/AcademicStaff.js
+++ api/models/AcademicStaff.js
@@ -26,31 +26,6 @@ module.exports = {
 		employeeNo: {
 			type: 'string',
 			size: 50
-		},
-		//Join table AcademicStaff_Department 1:M
-		AcademicStaff_Department: {
-			collection: 'AcademicStaff_Department',
-			via: 'academicStaffID'
-		},
-		//Join table AcademicStaff_Section 1:M
-		AcademicStaff_Section: {
-			collection: 'AcademicStaff_Section',
-			via: 'academicStaffID'
-		},
-		//has contract staffs 1:M
-		ContractStaff: {
-			collection: 'ContractStaff',
-			via: 'academicStaffID'
-		},
-		//has regular staffs 1:M
-		RegularStaff: {
-			collection: 'RegularStaff',
-			via: 'academicStaffID'
-		},
-		//teaching activites 1:M
-		TeachingActivities: {
-			collection: 'TeachingActivities',
-			via: 'academicStaffID'
 		}
 	},
 	tableName: 'AcademicStaff'
