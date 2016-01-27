e1e4b8b1f1e77c991428fa82ed40b1d6181c6200
daeyoun24 <daeyoun1506@gmail.com>
2016-01-27 09:50:48 +0000

    Angular material: remove error on cancel, hide datepicker input, etc...

--- assets/components/content/regularStaff/regularStaffController.js
+++ assets/components/content/regularStaff/regularStaffController.js
@@ -1,15 +1,15 @@
-application.controller('regularStaffController', function($scope, $http, _, regularStaffs, ModalLoader, AnchorScroll, RegularStaffSchema, AddRegularStaffForm, EditRegularStaffForm) {
+application.controller('regularStaffController', function($scope, $http, _, regularStaffs, ModalLoader, AnchorScroll) {
 
     /* Initialization */
     
-    $scope.schema = RegularStaffSchema;
-    $scope.form = AddRegularStaffForm;
-    initAddForm();
+    $scope.staff = {};
+    $scope.isEditing = false;
 
     $scope.gridOptions = {
         data: regularStaffs.data,
         multiSelect: false,
         enableRowHeaderSelection: false,
+        enableHorizontalScrollbar: 0,
         columnDefs: [{
             name: 'First Name',
             field: 'academicStaffID.firstName'
@@ -24,13 +24,12 @@ application.controller('regularStaffController', function($scope, $http, _, regu
             field: 'tenureDate',
             cellFilter: 'date:\'yyyy-MM-dd\''
         }, {
-            name: 'contApptDate',
+            name: 'Cont\' Appt\' Date',
             field: 'contApptDate',
             cellFilter: 'date:\'yyyy-MM-dd\''
         }, {
             name: 'Start Date',
             field: 'startDate',
-            type: 'date',
             cellFilter: 'date:\'yyyy-MM-dd\''
         }, {
             name: 'End Date',
@@ -39,67 +38,63 @@ application.controller('regularStaffController', function($scope, $http, _, regu
         }]
     };
 
-    $scope.tabs = {
-    };
-    
+    $scope.tabs = {};
+
     /* Generic functions: need minor tweaks for another view */
 
     $scope.gridOptions.onRegisterApi = function(gridApi) {
         gridApi.selection.on.rowSelectionChanged($scope, function(row) {
-            if (row.entity.regularStaffID === $scope.model.regularStaffID) {
+            if (row.entity.regularStaffID === $scope.staff.regularStaffID) {
                 row.isSelected = true;
                 $scope.gotoElement('details');
             }
             else {
                 $scope.row = row;
-                initEditForm(row);
+                $scope.staff = _.cloneDeep(row.entity);
+                $scope.isEditing = true;
             }
         });
     };
 
     $scope.addRow = function() {
-        initAddForm();
-        // open the form
-        $scope.model.switch = true;
+        $scope.staff = {};
+        $scope.isEditing = false;
         $scope.gotoElement('details');
     };
 
-    $scope.onSubmit = function(form) {
-        $scope.$broadcast('schemaFormValidate');
+    $scope.submit = function() {
         console.log("Submit success");
+
+        if ($scope.isEditing) {
+            // Do put request
+        }
+        else {
+            // Do post request
+        }
     };
 
     $scope.delete = function() {
-        ModalLoader.delete($scope.row, '/regularStaff/')
-            .result.then(function(data) {
-                if (angular.isObject(data))
-                    delete($scope.model);
-            });
+        console.log("Delete success");
+        // ModalLoader.delete($scope.row, '/regularStaff/', $scope.staff.regularStaffID)
+        //     .result.then(function(data) {
+        //         if (_.isObject(data)) {
+        //             $scope.staff = {};
+        //         }
+        //     });
     };
 
-    $scope.cancel = function() {
-        if ($scope.form === AddRegularStaffForm)
-            $scope.model = {};
-        if ($scope.form === EditRegularStaffForm)
-            angular.merge($scope.model, $scope.row.entity);
-
-        $scope.model.switch = false;
+    $scope.cancel = function(form) {
+        if ($scope.isEditing) {
+            _.merge($scope.staff, $scope.row.entity);    
+        }
+        else {
+            $scope.staff = {};
+        }
+        // remove errors
+        form.$setUntouched();
     };
 
     $scope.gotoElement = function(eID) {
         AnchorScroll.scrollTo(eID);
     };
-
-    function initAddForm() {
-        $scope.form = AddRegularStaffForm;
-        $scope.model = {};
-    }
-
-    function initEditForm(row) {
-        $scope.form = EditRegularStaffForm;
-        $scope.model = angular.copy(row.entity);
-        $scope.model.switch = false;
-        
-        $scope.staff = _.cloneDeep(row.entity);
-    }
 });
