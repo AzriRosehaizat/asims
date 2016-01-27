2e80210ff5beeb2c3647d3dc4c60de041d4527f8
daeyoun24 <daeyoun1506@gmail.com>
2016-01-27 02:06:56 +0000

    Angular material

--- api/controllers/RegularStaffController.js
+++ api/controllers/RegularStaffController.js
@@ -6,29 +6,6 @@
  */
 
 module.exports = {
-	test: function(req, res){
-		var id = req.param('id');
-		AcademicStaff.findOne({staffID: id})
-		.then(function(AcademicStaff){
-			if(AcademicStaff===undefined)
-			return res.json({notFound:true});
-			//Get departments
-			var departments = AcademicStaff_Department.find( { staffID : AcademicStaff.staffID } ).populate('departmentID').then(function(departments){
-	                console.log(departments);
-	                return departments;
-	        });
-			return [AcademicStaff.staffID, AcademicStaff.firstName, AcademicStaff.lastName, departments];
-
-		}).spread( function(staffID, firstName, lastName, departments ){
-	        return res.json({
-	        	'staffID': staffID,
-	            'firstName': firstName, 
-	          	'lastName': lastName,
-	            'departments': departments
-	        });
-	    }).catch(function(e) {
-	    	console.log("You must define an ID first");
-	    });
-	}
+	
 };
 
--- assets/components/navigationBar/navigationBarController.js
+++ assets/components/navigationBar/navigationBarController.js
@@ -1,8 +1,20 @@
-application.controller('navigationBarController', function($scope, Auth) {
-    
-    $scope.auth = Auth;
-    
-    $scope.logout = function() {
-      Auth.logout();
+application.controller('navigationBarController', function($scope, $mdSidenav, Auth) {
+
+  $scope.auth = Auth;
+
+  $scope.logout = function() {
+    Auth.logout();
+  };
+
+  $scope.toggleLeft = buildToggler('left');
+
+  function buildToggler(navID) {
+    return function() {
+      $mdSidenav(navID)
+        .toggle()
+        .then(function() {
+          // Do something..
+        });
     };
+  }
 });
