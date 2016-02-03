5f6d6120594f4d0934eae45e6f6114bb9839b6ba
azri <azri_ahmad@hotmail.com>
2016-02-03 03:39:35 +0000

    update styling, controllers, routing, templates of side bar menu

---
+++ assets/components/navLeftBar/navLeftBarService.js
@@ -0,0 +1,95 @@
+application
+  .factory('menu', [
+    '$location',
+    '$rootScope',
+    function($location) {
+
+      //The object for storing structure of the menu is just an array named 
+      // sections which allows us to visually see the structure easy and add items to really easy.
+      var sections = [{
+        name: 'Home',
+        state: 'application.root',
+        type: 'link'
+      }];
+
+      sections.push({
+        name: 'Application',
+        type: 'toggle',
+        pages: [{
+          name: 'Regular Staff',
+          type: 'link',
+          state: 'application.regularStaff'
+        }, {
+          name: 'Contract Staff',
+          state: 'application.contractStaff',
+          type: 'link'
+        }, {
+          name: 'Department',
+          state: 'application.department',
+          type: 'link'
+        }]
+      });
+
+      sections.push({
+        name: 'Report',
+        type: 'toggle',
+        pages: [{
+          name: 'Faculty Load Chart',
+          type: 'link',
+          state: 'application.report.FLC'
+        }, {
+          name: 'Other Report',
+          state: 'application.report.other',
+          type: 'link'
+        }]
+      });
+
+      sections.push({
+        name: 'Admin',
+        type: 'link',
+        state: 'application.admin'
+      });
+      var self;
+
+      return self = {
+        sections: sections,
+
+        toggleSelectSection: function(section) {
+          self.openedSection = (self.openedSection === section ? null : section);
+        },
+        isSectionSelected: function(section) {
+          return self.openedSection === section;
+        },
+
+        selectPage: function(section, page) {
+          page && page.url && $location.path(page.url);
+          self.currentSection = section;
+          self.currentPage = page;
+        }
+      };
+
+    }
+  ])
+
+//take all whitespace out of string
+.filter('nospace', function() {
+    return function(value) {
+      return (!value) ? '' : value.replace(/ /g, '');
+    };
+  })
+  //transform object to to readable links
+  .filter('humanizeDoc', function() {
+    return function(doc) {
+      if (!doc) return;
+      if (doc.type === 'directive') {
+        return doc.name.replace(/([A-Z])/g, function($1) {
+          return '-' + $1.toLowerCase();
+        });
+      }
+      return doc.label || doc.name;
+    };
+    // Helper function to use if want to sort alphabetically
+    function sortByName(a, b) {
+      return a.name < b.name ? -1 : 1;
+    }
+  });
---
+++ assets/components/navLeftBar/navLeftBarToggleDirective.js
@@ -0,0 +1,30 @@
+/*
+* menuToggle that shows and hides menu items that have sub-items assocaited with them
+*/
+application
+  .directive('menuToggle', ['$timeout', function ($timeout ) {
+    return {
+      scope: {
+        section: '='
+      },
+      // templateUrl: 'partials/menu-toggle.html',
+      templateUrl: 'components/navLeftBar/partials/navLeftBarToggle.tmpl.html',
+      link: function (scope, element) {
+        // allows to get a handle of the parent controller
+        var controller = element.parent().controller();
+
+        scope.isOpen = function () {
+          return controller.isOpen(scope.section);
+        };
+        scope.toggle = function () {
+          controller.toggleOpen(scope.section);
+        };
+
+        var parentNode = element[0].parentNode.parentNode.parentNode;
+        if (parentNode.classList.contains('parent-list-item')) {
+          var heading = parentNode.querySelector('h2');
+          element[0].firstChild.setAttribute('aria-describedby', heading.id);
+        }
+      }
+    };
+  }]);
