7156e603df2c20713995aa39695083b358f69083
anita01 <nelsonanita00@gmail.com>
2016-02-07 15:21:24 +0000

    Populate and Delete functionality

--- assets/components/toDoList/toDoListController.js
+++ assets/components/toDoList/toDoListController.js
@@ -1,5 +1,5 @@
 application.controller('toDoListController', function($scope, user, $http) {
-    $scope.user = user.data;
+    var userid = user.data.id;
     $scope.toDoListForm = {};
     
     $scope.hoverIn = function(){
@@ -10,15 +10,27 @@ application.controller('toDoListController', function($scope, user, $http) {
         this.hoverEdit = false;
     }
     
-    $http.get('/ToDoList')
+    var getUrl = 'ToDoList?userid=' + userid;
+    $http.get(getUrl)
         .success(function(data) {
             $scope.list = data;
         });
 
     $scope.addItem = function() {
-        http.post('/ToDoList/create', $scope.toDoListForm)
+        $http.post('/ToDoList/create?' + $scope.toDoListForm)
             .success(function(data) {
                 $scope.list = data;
+            });
+    }
+    
+    $scope.deleteItem = function(item){
+        $http.delete('ToDoList/' + item)
+            .then(function(data){
+                console.log('Item Deleted');
+                $http.get(getUrl)
+                    .success(function(data){
+                        $scope.list = data;
+                    });
             });
     }
 })
