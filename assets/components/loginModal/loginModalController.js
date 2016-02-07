fc6bee2fbdc0a06c103eb92201ceaf9e166108d7
daeyoun24 <daeyoun1506@gmail.com>
2016-02-07 05:09:26 +0000

    Added toast to login dialog and refactored the controller

--- assets/components/loginModal/loginModalController.js
+++ assets/components/loginModal/loginModalController.js
@@ -1,18 +1,12 @@
-application.controller('loginModalController', function($scope, $state, $mdDialog, Auth) {
+application.controller('loginModalController', function($scope, loginModalService) {
 
     $scope.formData = {};
 
     $scope.submit = function() {
-        Auth.login($scope.formData)
-            .then(function(res) {
-                $mdDialog.cancel();
-                $state.go("application.root");
-            }, function(err) {
-                console.warn(err);
-            });
+        loginModalService.submit($scope.formData);
     };
     
     $scope.cancel = function() {
-        $mdDialog.cancel();
+        loginModalService.cancel();
     };
 });
