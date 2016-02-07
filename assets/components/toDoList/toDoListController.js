2c0ee492a1688c0823f3031c7715a07b6b621133
anita01 <nelsonanita00@gmail.com>
2016-02-07 17:22:03 +0000

    Add and check item added

--- assets/components/toDoList/toDoListController.js
+++ assets/components/toDoList/toDoListController.js
@@ -1,15 +1,15 @@
 application.controller('toDoListController', function($scope, user, $http) {
     var userid = user.data.id;
     $scope.toDoListForm = {};
-    
-    $scope.hoverIn = function(){
+
+    $scope.hoverIn = function() {
         this.hoverEdit = true;
     }
-    
-    $scope.hoverOut = function(){
+
+    $scope.hoverOut = function() {
         this.hoverEdit = false;
     }
-    
+
     var getUrl = 'ToDoList?userid=' + userid;
     $http.get(getUrl)
         .success(function(data) {
@@ -17,20 +17,36 @@ application.controller('toDoListController', function($scope, user, $http) {
         });
 
     $scope.addItem = function() {
-        $http.post('/ToDoList/create?' + $scope.toDoListForm)
-            .success(function(data) {
-                $scope.list = data;
-            });
-    }
-    
-    $scope.deleteItem = function(item){
-        $http.delete('ToDoList/' + item)
-            .then(function(data){
-                console.log('Item Deleted');
+        $http.post('/ToDoList/create?text=' + $scope.addToDoText + '&userid=' + userid)
+            .then(function(data) {
+                console.log($scope.addToDoText);
+                $scope.addToDoText = '';
                 $http.get(getUrl)
-                    .success(function(data){
+                    .success(function(data) {
                         $scope.list = data;
                     });
             });
+    }
+
+    $scope.checkItem = function(item) {
+        var id = item.id;
+        var state = item.state;
+        var currentState = !state;
+        $http.put('ToDoList/update/' + id + '?state=' + currentState)
+            .then(function(data) {})
+    }
+
+    $scope.deleteItem = function(id) {
+        $http.delete('ToDoList/' + id)
+            .then(function(data) {
+                $http.get(getUrl)
+                    .success(function(data) {
+                        $scope.list = data;
+                    });
+            });
+    }
+
+    $scope.deleteCompleted = function(item) {
+
     }
 })
