9f1326d1114f217e5d0644fecd40408be486e8aa
anita01 <nelsonanita00@gmail.com>
2016-02-07 19:31:21 +0000

    Minor fixes

--- assets/components/toDoList/toDoListService.js
+++ assets/components/toDoList/toDoListService.js
@@ -1,12 +1,11 @@
 application.service('toDoListService', function($http) {
-    this.getList = function(userid) {
-        var getUrl = 'ToDoList?userid=' + userid;
-        var list;
-        $http.get(getUrl)
-            .success(function(data) {
-                list = data;
-            });
-        return list;
+    return {
+        getList: function(userid) {
+            var getUrl = 'ToDoList?userid=' + userid;
+            $http.get(getUrl)
+                .success(function(data) {
+                    return data;
+                });
+        }
     }
-
 })
