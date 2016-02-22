73aa4c35f6362a1a2fc5b835653a07f0bc6fe7f3
azri <azri_ahmad@hotmail.com>
2016-02-22 01:18:14 +0000

    Add teaching activities RAS

--- assets/components/content/regularStaff/regularStaffController.js
+++ assets/components/content/regularStaff/regularStaffController.js
@@ -7,7 +7,7 @@ application.controller('regularStaffController', function($scope, staffs, regula
     $scope.gridOptions = regularStaffService.gridOptions();
     $scope.gridOptions.data = $scope.rStaff;
     $scope.tabs = regularStaffService.tabs();
-    
+
     regularStaffService.initAddForm($scope.formData, $scope.gridOptions.data);
     SearchHelper.init($scope.gridOptions, $scope.rStaff);
 
@@ -20,7 +20,7 @@ application.controller('regularStaffController', function($scope, staffs, regula
                 $scope.row = row;
                 regularStaffService.initEditForm($scope.formData, row);
             }
-
+            regularStaffService.getTeachingActivity($scope.tabs.teachingActivity, row);
             regularStaffService.getDepartment($scope.tabs.department, row);
             regularStaffService.getRank($scope.tabs.rank, row);
             regularStaffService.getEmployment($scope.tabs.employment, row);
@@ -30,7 +30,7 @@ application.controller('regularStaffController', function($scope, staffs, regula
     $scope.addRow = function() {
         regularStaffService.initAddForm($scope.formData, $scope.gridOptions.data);
     };
-    
+
     $scope.editRow = function() {
         regularStaffService.initEditForm($scope.formData, $scope.row);
     };
