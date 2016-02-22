15c29598a330a28c91b7847f29e37659642bc30e
daeyoun24 <daeyoun1506@gmail.com>
2016-02-22 01:45:31 +0000

    Department tab

--- assets/components/content/regularStaff/regularStaffController.js
+++ assets/components/content/regularStaff/regularStaffController.js
@@ -1,4 +1,4 @@
-application.controller('regularStaffController', function($scope, staffs, regularStaffService, SearchHelper) {
+application.controller('regularStaffController', function($scope, staffs, regularStaffService, rsTabService, SearchHelper) {
 
     $scope.gridTitle = 'Regular Staff';
     $scope.rStaff = staffs.data;
@@ -7,33 +7,45 @@ application.controller('regularStaffController', function($scope, staffs, regula
     $scope.gridOptions = regularStaffService.gridOptions();
     $scope.gridOptions.data = $scope.rStaff;
     $scope.tabs = regularStaffService.tabs();
-    
+    $scope.tabData = {};
+
     regularStaffService.initAddForm($scope.formData, $scope.gridOptions.data);
     SearchHelper.init($scope.gridOptions, $scope.rStaff);
 
     $scope.gridOptions.onRegisterApi = function(gridApi) {
         gridApi.selection.on.rowSelectionChanged($scope, function(row) {
-            if (row.entity.academicStaffID === $scope.formData.model.academicStaffID) {
-                row.isSelected = true;
-            }
-            else {
-                $scope.row = row;
-                regularStaffService.initEditForm($scope.formData, row);
-            }
+            $scope.row = row;
+            regularStaffService.initEditForm($scope.formData, row);
 
             regularStaffService.getDepartment($scope.tabs.department, row);
             regularStaffService.getRank($scope.tabs.rank, row);
             regularStaffService.getEmployment($scope.tabs.employment, row);
-            
-            
         });
     };
 
     $scope.addRow = function() {
         regularStaffService.initAddForm($scope.formData, $scope.gridOptions.data);
     };
-    
+
     $scope.editRow = function() {
         regularStaffService.initEditForm($scope.formData, $scope.row);
+    };
+
+    $scope.tabs.department.gridOptions.onRegisterApi = function(gridApi) {
+        gridApi.selection.on.rowSelectionChanged($scope, function(row) {
+            rsTabService.initEditForm($scope.tabData, row);
+        });
+    };
+
+    $scope.tabs.rank.gridOptions.onRegisterApi = function(gridApi) {
+        gridApi.selection.on.rowSelectionChanged($scope, function(row) {
+            
+        });
+    };
+
+    $scope.tabs.employment.gridOptions.onRegisterApi = function(gridApi) {
+        gridApi.selection.on.rowSelectionChanged($scope, function(row) {
+            
+        });
     };
 });
---
+++ assets/components/content/regularStaff/rsTabService.js
@@ -0,0 +1,66 @@
+application.service('rsTabService', function($http, $q, _, formService) {
+
+    // Department
+    return {
+        initAddForm: function(formData, gridData) {
+            formData.model = {};
+            formData.isEditing = false;
+            formData.title = 'Add Department';
+            formData.inputs = [{
+                type: "text",
+                name: "title",
+                label: "Name",
+                disabled: false,
+                required: true
+            }, {
+                type: "date",
+                name: "startDate",
+                label: "Start date",
+                disabled: false,
+                required: false
+            }, {
+                type: "date",
+                name: "endDate",
+                label: "End date",
+                disabled: false,
+                required: false
+            }];
+
+            formService.setGridData(gridData);
+            formService.setFormData(formData, 'rsTabService');
+        },
+        initEditForm: function(formData, row) {
+            formData.model = _.cloneDeep(row.entity);
+            formData.isEditing = true;
+            formData.title = 'Edit Department';
+            formData.inputs = [{
+                type: "text",
+                name: "departmentCode",
+                label: "Code",
+                disabled: true,
+                required: true
+            }, {
+                type: "text",
+                name: "title",
+                label: "Name",
+                disabled: false,
+                required: true
+            }, {
+                type: "date",
+                name: "startDate",
+                label: "Start date",
+                disabled: false,
+                required: false
+            }, {
+                type: "date",
+                name: "endDate",
+                label: "End date",
+                disabled: false,
+                required: false
+            }];
+
+            formService.setRow(row);
+            formService.setFormData(formData, 'rsTabService');
+        },
+    };
+});
