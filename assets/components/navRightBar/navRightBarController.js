cf04fcdba6b66f1d0f8876d8cbacd2ef633fb75e
daeyoun24 <daeyoun1506@gmail.com>
2016-02-24 21:49:18 -0600

    not using formController for now

--- assets/components/navRightBar/navRightBarController.js
+++ assets/components/navRightBar/navRightBarController.js
@@ -1,5 +1,5 @@
-application.controller('navRightBarController', function($scope, formService) {
-
+application.controller('navRightBarController', function($scope, $http, _, formService) {
+    
     $scope.fs = formService;
 
     $scope.submit = function() {
@@ -12,5 +12,17 @@ application.controller('navRightBarController', function($scope, formService) {
 
     $scope.delete = function(ev) {
         formService.delete(ev, $scope.fs.formData);
+    };
+
+    $scope.querySearch = function(query, url, output) {
+        return $http.get(url + query + "\"}}")
+            .then(function(res) {
+                return _.map(res.data, function(item) {
+                    return {
+                        obj: item,
+                        name: item[output.name]
+                    };
+                });
+            });
     };
 });
