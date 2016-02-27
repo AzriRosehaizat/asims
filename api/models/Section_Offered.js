0dbb971916f2cd437a1e2d9575196d2ec4f74226
daeyoun24 <daeyoun1506@gmail.com>
2016-02-27 01:07:39 -0600

    TA tab WIP

--- api/models/Section_Offered.js
+++ api/models/Section_Offered.js
@@ -23,9 +23,8 @@ module.exports = {
 			required: true
 		},
 		groupID: {
-			type: 'integer'
-			// ,
-			// required: true
+			type: 'integer',
+			required: true
 		},
 		startDate:{
 			type: 'date',
@@ -33,8 +32,7 @@ module.exports = {
 		},
 		endDate:{
 			type: 'date',
-			required: true 
-
+			required: true
 		},
 		TeachingActivities: {
 			collection: 'TeachingActivities',
