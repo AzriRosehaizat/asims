26d7afba59a600e242d0001d878d6db340353767
anita01 <nelsonanita00@gmail.com>
2016-02-28 23:42:59 +0000

    Urls for Main and related grids

--- api/controllers/DepartmentController.js
+++ api/controllers/DepartmentController.js
@@ -6,5 +6,36 @@
  */
 
 module.exports = {
-};
 
+    getAllDepartment: function(req, res) {
+        var departmentID = req.param('departmentID');
+        DepartmentService.getAllDepartment(departmentID, function(err, result) {
+            if (err) return res.serverError(err);
+            return res.ok(result);
+        });
+    },
+    getInfo: function(req, res) {
+        var responseFn = function(err, result) {
+            if (err) return res.serverError(err);
+            return res.ok(result);
+        };
+        var data = {
+            id: req.param('id'),
+            type: req.param('type')
+        };
+        switch (data.type) {
+            case 'course':
+                DepartmentService.getCourse(data.id, responseFn)
+                break;
+            case 'regularStaff':
+                DepartmentService.getRegularStaff(data.id, responseFn)
+                break;
+            case 'contractStaff':
+                DepartmentService.getContractStaff(data.id, responseFn)
+                break;
+            default:
+                res.serverError();
+                console.log("Incorrect REST url");
+        }
+    }
+};
