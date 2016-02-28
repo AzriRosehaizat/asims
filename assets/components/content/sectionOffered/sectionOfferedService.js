3786254a39e4d36e8b925d310d05d53c968a9265
azri <azri_ahmad@hotmail.com>
2016-02-28 17:42:44 -0600

    edit works section offered'

--- assets/components/content/sectionOffered/sectionOfferedService.js
+++ assets/components/content/sectionOffered/sectionOfferedService.js
@@ -28,7 +28,23 @@ application.service('sectionOfferedService', function($http, _, formService) {
             };
         },
         update: function(formData) {
-            return $http.post('', formData.model);
+            if (_.isObject(formData.model.departmentCode) &&
+                _.isObject(formData.model.courseNo) &&
+                _.isObject(formData.model.sectionNo)) {
+
+                //Define the IDs
+                formData.model.courseID = formData.model.courseNo.obj.courseID;
+                formData.model.sectionID = formData.model.sectionNo.obj.sectionID;
+            }
+            
+            //Update section offered
+            return $http.put('/Section_Offered/' + formData.model.sectionOfferedID, formData.model)
+                .then(function(res) {
+                    return $http.get('/Section_Offered/getSectionOffered/' + res.data.sectionOfferedID)
+                        .then(function(SO) {
+                            return SO;
+                        });
+                });
         },
         create: function(formData) {
             if (_.isObject(formData.model.departmentCode) &&
@@ -156,7 +172,87 @@ application.service('sectionOfferedService', function($http, _, formService) {
             formData.isEditing = true;
             formData.title = 'Edit Section Offered';
             formData.inputs = [{
-
+                //This is an example for md-autocomplete/dropdown
+                type: "autocomplete",
+                //object name
+                name: "departmentCode",
+                //Caption
+                label: "Dept. Code",
+                //URL to parse the dropdown items
+                url: {
+                    start: "/department?where={",
+                    end: "\"departmentCode\":{\"startsWith\":\""
+                },
+                //for UI-sref linking 
+                link: "application.department",
+                output: {
+                    obj: {},
+                    name: "departmentCode"
+                },
+                change: {
+                    to: "courseNo"
+                },
+                disabled: false,
+                required: true
+            }, {
+                type: "autocomplete",
+                name: "courseNo",
+                label: "Course No.",
+                url: {
+                    start: "/course?where={",
+                    end: "\"courseNo\":{\"startsWith\":\"",
+                    where: [{
+                        key: "departmentID",
+                        value: "departmentCode.obj.departmentID"
+                    }]
+                },
+                link: "application.course",
+                output: {
+                    obj: {},
+                    name: "courseNo"
+                },
+                change: {
+                    to: "sectionNo"
+                },
+                disabled: "!isObject('departmentCode')",
+                required: true
+            }, {
+                type: "autocomplete",
+                name: "sectionNo",
+                label: "Section No.",
+                url: {
+                    start: "/Section?where={",
+                    end: "\"sectionNo\":{\"startsWith\":\""
+                },
+                link: "application.section",
+                output: {
+                    obj: {},
+                    name: "sectionNo"
+                },
+                change: {
+                    from: "courseNo.obj.title",
+                    to: "title"
+                },
+                disabled: "!isObject('courseNo')",
+                required: true
+            }, {
+                type: "date",
+                name: "startDate",
+                label: "Start Date",
+                required: true,
+                disabled: false
+            }, {
+                type: "date",
+                name: "endDate",
+                label: "End Date",
+                required: true,
+                disabled: false
+            }, {
+                type: "text",
+                name: "title",
+                label: "Title",
+                disabled: true,
+                required: true
             }];
 
             formService.init(formData, gridData, row, 'sectionOfferedService', true);
