a69562472659914c9b4cc9526bc78c3e8b756622
anita01 <nelsonanita00@gmail.com>
2016-03-01 22:42:02 +0000

    Sidebar order

--- assets/components/theme/theme.js
+++ assets/components/theme/theme.js
@@ -5,7 +5,7 @@ application.config(function($mdThemingProvider) {
         .warnPalette('red', {
             'hue-2': '900'
         })
-        .accentPalette('indigo')
+        .accentPalette('grey')
         .backgroundPalette('grey');
 
     $mdThemingProvider.theme('dark')
