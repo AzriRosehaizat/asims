678233ed78ad1595b13e3ca73b2d5f8057ac45ae
anita01 <nelsonanita00@gmail.com>
2016-03-01 03:10:34 +0000

    Rank Page creatAPI

---
+++ assets/components/content/rank/rankService.js
@@ -0,0 +1,69 @@
+application.service('rankService', function($http, _, formService) {
+    
+    return {
+        gridOptions: function() {
+            return {
+                columnDefs: [{
+                    name: 'Title',
+                    field: 'title'
+                }, {
+                    name: 'Description',
+                    field: 'description'
+                }]
+            };
+        },
+        update: function(formData) {
+            return $http.put('/rank/', formData.model.rankID, formData.model);
+        },
+        create: function(formData) {
+            return $http.post('/rank/', formData.model);
+        },
+        delete: function(formData) {
+            return $http.delete('/rank/', formData.model.rankID);
+        },
+        initAddForm: function(formData, gridData) {
+            formData.model = {};
+            formData.isEditing = false;
+            formData.title = 'Add Rank';
+            formData.inputs = [{
+                type: "text",
+                name: "title",
+                label: "Title",
+                disabled: false,
+                required: true
+            }, {
+                type: "textarea",
+                name: "description",
+                label: "Description",
+                disabled: false,
+                required: true
+            }];
+
+            formService.init(formData, gridData, null, 'rankService', true);
+        },
+        initEditForm: function(formData, gridData, row) {
+            formData.model = _.cloneDeep(row.entity);
+            formData.isEditing = true;
+            formData.title = 'Edit Rank';
+            formData.inputs = [{
+                type: "text",
+                name: "title",
+                label: "Title",
+                disabled: false,
+                required: true
+            }, {
+                type: "textarea",
+                name: "description",
+                label: "Description",
+                disabled: false,
+                required: true
+            }];
+
+            formService.init(formData, gridData, row, 'rankService', true);
+        },
+        getRow: function(row) {
+            console.log(row.entity.rankID);
+            return $http.get('/rank/' + row.entity.rankID);
+        }
+    };
+});
