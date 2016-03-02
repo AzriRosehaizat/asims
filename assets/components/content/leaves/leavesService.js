e6d2cdf59f7eb2552f52eab5f97ff44e39367618
james10011 <jamesmackay10011@gmail.com>
2016-03-02 17:49:52 +0000

    added a leaves page, and preliminary functionality

---
+++ assets/components/content/leaves/leavesService.js
@@ -0,0 +1,39 @@
+/* global
+    application
+*/
+
+application
+.service(
+    'leavesService', 
+    function(
+        $http, 
+        _, 
+        formService
+    ){
+        return {
+            gridOptions : {
+                columnDefs: [{
+                    name: 'First Name',
+                    field: 'firstName'
+                }, {
+                    name: 'Last Name',
+                    field: 'lastName'
+                }, {
+                    name: 'Department',
+                    field: 'departmentCode'
+                }, {
+                    name: 'Rank',
+                    field: 'Rank'
+                }, {
+                    name: 'Tenure Date',
+                    field: 'tenureDate',
+                    cellFilter: 'date:\'yyyy-MM-dd\''
+                }, {
+                    name: 'Cont\' appointment date',
+                    field: 'contAppDate',
+                    cellFilter: 'date:\'yyyy-MM-dd\''
+                }]
+            }
+        };
+    }
+);
