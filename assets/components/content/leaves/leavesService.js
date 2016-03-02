a8b6e7583c7dc0ecfa9a8232f93ba709831f1dc0
james10011 <jamesmackay10011@gmail.com>
2016-03-02 18:16:54 +0000

    added row selection handler

--- assets/components/content/leaves/leavesService.js
+++ assets/components/content/leaves/leavesService.js
@@ -12,27 +12,43 @@ application
     ){
         return {
             gridOptions : {
-                columnDefs: [{
-                    name: 'First Name',
-                    field: 'firstName'
-                }, {
-                    name: 'Last Name',
-                    field: 'lastName'
-                }, {
-                    name: 'Department',
-                    field: 'departmentCode'
-                }, {
-                    name: 'Rank',
-                    field: 'Rank'
-                }, {
-                    name: 'Tenure Date',
-                    field: 'tenureDate',
-                    cellFilter: 'date:\'yyyy-MM-dd\''
-                }, {
-                    name: 'Cont\' appointment date',
-                    field: 'contAppDate',
-                    cellFilter: 'date:\'yyyy-MM-dd\''
-                }]
+                columnDefs: [
+                    {
+                        name: 
+                            'First Name',
+                        field: 
+                            'firstName'
+                    }, {
+                        name: 
+                            'Last Name',
+                        field: 
+                            'lastName'
+                    }, {
+                        name: 
+                            'Department',
+                        field: 
+                            'departmentCode'
+                    }, {
+                        name: 
+                            'Rank',
+                        field: 
+                            'Rank'
+                    }, {
+                        name: 
+                            'Tenure Date',
+                        field: 
+                            'tenureDate',
+                        cellFilter: 
+                            'date:\'yyyy-MM-dd\''
+                    }, {
+                        name: 
+                            'Cont\' appointment date',
+                        field: 
+                            'contAppDate',
+                        cellFilter: 
+                            'date:\'yyyy-MM-dd\''
+                    }
+                ]
             }
         };
     }
--- assets/components/content/leaves/tabs/leavesCredits.js
+++ assets/components/content/leaves/tabs/leavesCredits.js
@@ -12,7 +12,9 @@ application
     ){
         return {
             gridOptions : {
-                columnDefs: []
+                columnDefs: [
+                
+                ]
             }
         };
     }
