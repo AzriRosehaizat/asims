d442b37d7c7e70ff6ebce727ea0c91aa04959364
james10011 <jamesmackay10011@gmail.com>
2016-03-08 02:43:46 +0000

    reformatted RegularStaff Service Code

--- api/services/RegularStaffService.js
+++ api/services/RegularStaffService.js
@@ -151,19 +151,24 @@ module.exports = {
 	getLeaveCredits: function(id, where, callback) {
 		var sql;
 
-		sql = mysql.select('*').from('LeaveCredit')
-			.where(Object.assign({
-				regularStaffID: id
-			}, where || {}));
-
-		LeaveCredit.query(sql.toString(), callback);
-
-		// sql = mysql.select('lc.*')
-		// 	.from('LeaveCredit AS lc')
-		// 	.where('lc.regularStaffID', id);
-
-		// sql = (where) ? sql.where('lc.leaveCreditID', where).toString() : sql.toString();
-		// LeaveCredit.query(sql, callback);
+		sql = (
+			mysql
+			.select('*')
+			.from('LeaveCredit')
+			.where(
+				Object.assign(
+					{
+						regularStaffID: id
+					}, 
+					where || {}
+				)
+			)
+		); 
+		LeaveCredit
+		.query(
+			sql.toString(), 
+			callback
+		);
 	},
 
 
@@ -177,18 +182,23 @@ module.exports = {
 	getLeaveDebits: function(id, where, callback) {
 		var sql;
 
-		sql = mysql.select('*').from('LeaveDebit')
-			.where(Object.assign({
-				regularStaffID: id
-			}, where || {}));
-
-		LeaveDebit.query(sql.toString(), callback);
-
-		// sql = mysql.select('ld.*')
-		// 	.from('LeaveDebit AS ld')
-		// 	.where('ld.regularStaffID', id);
-
-		// sql = (where) ? sql.where('ld.leaveDebitID', where).toString() : sql.toString();
-		// LeaveDebit.query(sql, callback);
+		sql = (
+			mysql
+			.select('*')
+			.from('LeaveDebit')
+			.where(
+				Object.assign(
+					{
+						regularStaffID: id
+					}, where || {}
+				)
+			)
+		);
+		
+		LeaveDebit
+		.query(
+			sql.toString(), 
+			callback
+		);
 	}
 };
