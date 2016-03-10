c5c6a6c7bb8ab0756122b6097a22cb97b98f7378
Azri Ahmad <AzriAhmad@users.noreply.github.com>
2016-03-10 16:36:15 -0600

    Revert "related tab redirect to related main page"

--- assets/components/content/department/dTabService.js
+++ assets/components/content/department/dTabService.js
@@ -5,7 +5,6 @@ application.service('dTabService', function($http, dCourse, dChair, dRegularStaf
             return {
                 course: {
                     title: 'Course',
-                    link: 'application.course',
                     gridOptions: {
                         columnDefs: [{
                             name: 'Department',
@@ -22,7 +21,6 @@ application.service('dTabService', function($http, dCourse, dChair, dRegularStaf
                 },
                 regularStaff: {
                     title: 'Regular Staff',
-                    link: 'application.regularStaff',
                     gridOptions: {
                         columnDefs: [{
                             name: 'First Name',
@@ -54,7 +52,6 @@ application.service('dTabService', function($http, dCourse, dChair, dRegularStaf
                 },
                 contractStaff: {
                     title: 'Contract Staff',
-                    link: 'application.contractStaff',
                     gridOptions: {
                         columnDefs: [{
                             name: 'First Name',
