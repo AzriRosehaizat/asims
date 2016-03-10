3d70f37948bc2a8325467f89eb78245fa3c310e6
azri <azri_ahmad@hotmail.com>
2016-03-10 16:54:16 -0600

    fix

--- assets/components/content/course/cTabService.js
+++ assets/components/content/course/cTabService.js
@@ -1,24 +1,20 @@
-application.service('csTabService', function($http, csTA, csDepartment, csRank, csEmployment) {
+application.service('cTabService', function($http, cSection) {
 
     return {
         tabs: function() {
             return {
-                teachingActivity: {
-                    title: 'Teaching Activity',
-                    link: 'application.teachingActivityCAS',
+                section: {
+                    title: 'Teaching History',
                     gridOptions: {
                         columnDefs: [{
-                            name: 'Dept. Code',
-                            field: 'departmentCode'
+                            name: 'First Name',
+                            field: 'firstName'
                         }, {
-                            name: 'Course No.',
-                            field: 'courseNo',
+                            name: 'Last Name',
+                            field: 'lastName'
                         }, {
                             name: 'Section No.',
-                            field: 'sectionNo',
-                        }, {
-                            name: 'Title',
-                            field: 'title',
+                            field: 'sectionNo'
                         }, {
                             name: 'Term',
                             field: 'term',
@@ -34,130 +30,37 @@ application.service('csTabService', function($http, csTA, csDepartment, csRank, 
                             field: 'endDate',
                             cellFilter: 'date:\'MM-dd-yyyy\''
                         }, {
-                            name: 'FCE',
-                            displayName: 'FCE',
-                            field: 'FCEValue'
-                        }, {
                             name: 'Role',
                             field: 'role'
                         }]
-                    }
-                },
-                department: {
-                    title: 'Department',
-                    link: 'application.department',
-                    gridOptions: {
-                        columnDefs: [{
-                            name: 'Code',
-                            field: 'departmentCode'
-                        }, {
-                            name: 'Name',
-                            field: 'title'
-                        }, {
-                            name: 'Start Date',
-                            field: 'startDate',
-                            cellFilter: 'date:\'MM-dd-yyyy\''
-                        }, {
-                            name: 'End Date',
-                            field: 'endDate',
-                            cellFilter: 'date:\'MM-dd-yyyy\''
-                        }]
-                    }
-                },
-                rank: {
-                    title: 'Rank',
-                    link: 'application.rank',
-                    gridOptions: {
-                        columnDefs: [{
-                            name: 'Name',
-                            field: 'title'
-                        }, {
-                            name: 'Start Date',
-                            field: 'startDate',
-                            cellFilter: 'date:\'MM-dd-yyyy\''
-                        }, {
-                            name: 'End Date',
-                            field: 'endDate',
-                            cellFilter: 'date:\'MM-dd-yyyy\''
-                        }]
-                    }
-                },
-                employment: {
-                    title: 'Employment',
-                    gridOptions: {
-                        columnDefs: [{
-                            name: 'Start Date',
-                            field: 'startDate',
-                            cellFilter: 'date:\'MM-dd-yyyy\''
-                        }, {
-                            name: 'End Date',
-                            field: 'endDate',
-                            cellFilter: 'date:\'MM-dd-yyyy\''
-                        }]
-                    }
+                    },
+                    readOnly: true
                 }
             };
         },
         initAddForm: function(formData, tab, mainRow) {
             switch (tab.title) {
-                case 'Teaching Activity':
-                    csTA.initAddForm(formData, tab.gridOptions.data, mainRow);
-                    break;
-                case 'Department':
-                    csDepartment.initAddForm(formData, tab.gridOptions.data, mainRow);
-                    break;
-                case 'Rank':
-                    csRank.initAddForm(formData, tab.gridOptions.data, mainRow);
-                    break;
-                case 'Employment':
-                    csEmployment.initAddForm(formData, tab.gridOptions.data, mainRow);
+                case 'Teaching History':
+                    tab.gridOptions.data.readOnly = tab.readOnly;
+                    cSection.initAddForm(formData, tab.gridOptions.data, mainRow);
                     break;
             }
         },
         initEditForm: function(formData, tab, row) {
             switch (tab.title) {
-                case 'Teaching Activity':
-                    csTA.initEditForm(formData, tab.gridOptions.data, row);
-                    break;
-                case 'Department':
-                    csDepartment.initEditForm(formData, tab.gridOptions.data, row);
-                    break;
-                case 'Rank':
-                    csRank.initEditForm(formData, tab.gridOptions.data, row);
-                    break;
-                case 'Employment':
-                    csEmployment.initEditForm(formData, tab.gridOptions.data, row);
+                case 'Teaching History':
+                    tab.gridOptions.data.readOnly = tab.readOnly;
+                    cSection.initEditForm(formData, tab.gridOptions.data, row);
                     break;
             }
         },
         getTabs: function(tabs, row) {
-            this.getTeachingActivity(tabs.teachingActivity, row);
-            this.getDepartment(tabs.department, row);
-            this.getRank(tabs.rank, row);
-            this.getEmployment(tabs.employment, row);
+            this.getSection(tabs.section, row);
         },
-        getTeachingActivity: function(teachingActivity, row) {
-            $http.get('/contractStaff/getInfo?type=teaching&id=' + row.entity.academicStaffID)
+        getSection: function(section, row) {
+            $http.get('/course/getInfo?type=section&id=' + row.entity.courseID)
                 .then(function(res) {
-                    teachingActivity.gridOptions.data = res.data;
-                });
-        },
-        getDepartment: function(department, row) {
-            $http.get('/contractStaff/getInfo?type=department&id=' + row.entity.academicStaffID)
-                .then(function(res) {
-                    department.gridOptions.data = res.data;
-                });
-        },
-        getRank: function(rank, row) {
-            $http.get('/contractStaff/getInfo?type=rank&id=' + row.entity.academicStaffID)
-                .then(function(res) {
-                    rank.gridOptions.data = res.data;
-                });
-        },
-        getEmployment: function(employment, row) {
-            $http.get('/contractStaff/getInfo?type=employment&id=' + row.entity.academicStaffID)
-                .then(function(res) {
-                    employment.gridOptions.data = res.data;
+                    section.gridOptions.data = res.data;
                 });
         }
     };
