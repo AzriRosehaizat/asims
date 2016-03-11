bb5c24467652ab5af69241aa25a306887bbf8547
daeyoun24 <daeyoun1506@gmail.com>
2016-03-10 21:27:13 -0600

    Removed disabled

--- assets/components/content/leaves/leaveService.js
+++ assets/components/content/leaves/leaveService.js
@@ -3,127 +3,93 @@
 */
 
 application
-.service(
-    'leaveService', 
-    function(
-        $http, 
-        _, 
-        formService,
-        $q
-    ){
-        return {
-            gridOptions : {
-                columnDefs: [
-                    {
-                        name: 
-                            'First Name',
-                        field: 
-                            'firstName'
+    .service(
+        'leaveService',
+        function(
+            $http,
+            _,
+            formService,
+            $q
+        ) {
+            return {
+                gridOptions: {
+                    columnDefs: [{
+                        name: 'First Name',
+                        field: 'firstName'
                     }, {
-                        name: 
-                            'Last Name',
-                        field: 
-                            'lastName'
+                        name: 'Last Name',
+                        field: 'lastName'
                     }, {
-                        name: 
-                            'Department',
-                        field: 
-                            'departmentCode'
+                        name: 'Department',
+                        field: 'departmentCode'
                     }, {
-                        name: 
-                            'Rank',
-                        field: 
-                            'Rank'
-                    }
-                ],
-                readOnly:true
-            },
-            setForm: function( formData, gridData, row ) {
-                gridData
-                .readOnly = (
-                    true
-                );
-                
-                formData
-                .model = (
-                    _
-                    .cloneDeep(
-                        row
-                        .entity
-                    )
-                );
-                
-                formData
-                .isEditing = (
-                    true
-                );
-                
-                formData
-                .title = (
-                    'Regular Staff Details'
-                );
-                
-                formData
-                .inputs = [
-                    {
-                        type: 
-                            "text",
-                        name: 
-                            "firstName",
-                        label: 
-                            "First name",
-                        disabled: 
+                        name: 'Rank',
+                        field: 'Rank'
+                    }],
+                    readOnly: true
+                },
+                setForm: function(formData, gridData, row) {
+                    gridData
+                        .readOnly = (
                             true
-                    }, {
-                        type: 
-                            "text",
-                        name: 
-                            "lastName",
-                        label: 
-                            "Last name",
-                        disabled: 
+                        );
+
+                    formData
+                        .model = (
+                            _
+                            .cloneDeep(
+                                row
+                                .entity
+                            )
+                        );
+
+                    formData
+                        .isEditing = (
                             true
-                    }, {
-                        type: 
-                            "number",
-                        name: 
-                            "researchBalance",
-                        label: 
-                            "Research Balance",
-                        disabled: 
+                        );
+
+                    formData
+                        .title = (
+                            'Regular Staff Details'
+                        );
+
+                    formData
+                        .inputs = [{
+                            type: "text",
+                            name: "firstName",
+                            label: "First name"
+                        }, {
+                            type: "text",
+                            name: "lastName",
+                            label: "Last name"
+                        }, {
+                            type: "number",
+                            name: "researchBalance",
+                            label: "Research Balance"
+                        }, {
+                            type: "number",
+                            name: "administrativeBalance",
+                            label: "Administrative Balance"
+                        }];
+
+                    formService
+                        .init(
+                            formData,
+                            gridData,
+                            row,
+                            'leaveService',
                             true
-                    }, {
-                        type: 
-                            "number",
-                        name: 
-                            "administrativeBalance",
-                        label: 
-                            "Administrative Balance",
-                        disabled: 
-                            true
-                    }
-                ];
-                
-                formService
-                .init( 
-                    formData, 
-                    gridData, 
-                    row, 
-                    'leaveService', 
-                    true
-                );
-            },
-            getRow: function( row ) {
-                return $q
-                .resolve(
-                    {
-                        data: [
-                            row
-                            .entity
-                        ]
-                    } 
-                );
-            }
-        };
-    }
-);
+                        );
+                },
+                getRow: function(row) {
+                    return $q
+                        .resolve({
+                            data: [
+                                row
+                                .entity
+                            ]
+                        });
+                }
+            };
+        }
+    );
