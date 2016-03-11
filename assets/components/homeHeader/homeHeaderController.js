8b59d7e7aea070414b826a30fffd464cd0938c12
daeyoun24 <daeyoun1506@gmail.com>
2016-03-10 20:57:10 -0600

    Fixed welcome new user not showing

--- assets/components/homeHeader/homeHeaderController.js
+++ assets/components/homeHeader/homeHeaderController.js
@@ -2,9 +2,9 @@ application.controller('homeHeaderController', function($http, $scope, user, mom
 
     $scope.user = user.data;
 
-    //Get last login information
+    // Get last login information
     $http.get('/User/' + user.data.id + '/attempts?where={"successful":true}&sort=createdAt DESC&limit=2')
         .then(function(res) {
-            $scope.lastLogin = (res.data[1].createdAt) ? moment(res.data[1].createdAt).fromNow() : "Welcome New User";
+            $scope.lastLogin = (res.data[1]) ? moment(res.data[1].createdAt).fromNow() : "Welcome New User";
         });
 });
