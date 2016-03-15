3d9ec2ddda4c57dc02149124275b864ba70dde00
daeyoun24 <daeyoun1506@gmail.com>
2016-03-14 19:24:14 -0500

    Removed gridConfig

---
+++ assets/components/form/formConfig.js
@@ -0,0 +1,11 @@
+application.config(function($mdDateLocaleProvider, moment) {
+    
+    $mdDateLocaleProvider.formatDate = function(date) {
+        return moment(date).format('MM-DD-YYYY');
+    };
+
+    $mdDateLocaleProvider.parseDate = function(dateString) {
+        var m = moment(dateString, 'MM-DD-YYYY', true);
+        return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+});
--- assets/components/form/formController.js
+++
@@ -1,3 +0,0 @@
-application.controller('formController', function($scope) {
-
-});
