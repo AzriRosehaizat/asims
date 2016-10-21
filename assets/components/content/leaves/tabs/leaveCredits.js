49196903b629fe0cc7e86521f0c4626a6bfd0aa3
daeyoun24 <daeyoun1506@gmail.com>
2016-10-20 21:33:25 -0500

    reformatted

--- assets/components/content/leaves/tabs/leaveCredits.js
+++ assets/components/content/leaves/tabs/leaveCredits.js
@@ -1,242 +1,108 @@
-/* global
-    application
-*/
+/* global application */
 
-application
-.service(
-    'leaveCredits', 
-    function(
-        $http, 
-        _, 
-        uiGridConstants,
-        formService,
-        gridService
-    ){
-        return {
-            gridOptions : function ( $scope ){
-                return {
-                    showColumnFooter: true,
-                    columnDefs: [
-                        {
-                            name: 
-                                'Credit',
-                            field: 
-                                'amount',
-                            aggregationType: 
-                                uiGridConstants
-                                .aggregationTypes
-                                .sum
-                        }, {
-                            name: 
-                                'Start Date',
-                            field: 
-                                'startDate',
-                            cellFilter: 
-                                'date:\'MM-dd-yyyy\''
-                        }, {
-                            name: 
-                                'End Date',
-                            field: 
-                                'endDate',
-                            cellFilter: 
-                                'date:\'MM-dd-yyyy\''
-                        }, {
-                            name: 
-                                'Type',
-                            field: 
-                                'leaveCreditType'
-                        }
-                    ],
-                    onRegisterApi : function( gridApi ){
-                        gridService.set(
-                            gridApi, 
-                            'leaveCredit'
-                        );
-                        
-                        (function( gridOptions ){
-                            gridApi
-                            .selection
-                            .on
-                            .rowSelectionChanged( 
-                                null, 
-                                function( row ){
-                                    initializeEdit(
-                                        $scope
-                                        .formData,
-                                        gridOptions
-                                        .data,
-                                        row
-                                    );
-                                }
-                            );
-                        })(this);
-                    }
-                };
-            },
-            initAddForm: initializeAdd,
-            initEditForm: initializeEdit,
-            create: create,
-            update: update,
-            delete: remove
-        };
-        
-        function initializeEdit( formData, gridData, row ){
+application.service('leaveCredits', function($http, _, uiGridConstants, formService, gridService) {
+    return {
+        gridOptions: function($scope) {
+            return {
+                showColumnFooter: true,
+                columnDefs: [{
+                    name: 'Credit',
+                    field: 'amount',
+                    aggregationType: uiGridConstants.aggregationTypes.sum
+                }, {
+                    name: 'Start Date',
+                    field: 'startDate',
+                    cellFilter: 'date:\'MM-dd-yyyy\''
+                }, {
+                    name: 'End Date',
+                    field: 'endDate',
+                    cellFilter: 'date:\'MM-dd-yyyy\''
+                }, {
+                    name: 'Type',
+                    field: 'leaveCreditType'
+                }],
+                onRegisterApi: function(gridApi) {
+                    gridService.set(gridApi, 'leaveCredit');
 
-            formData
-            .model = (
-                _
-                .cloneDeep(
-                    row
-                    .entity
-                )
-            );
-            
-            formData
-            .isEditing = (
-                true
-            );
-            
-            formData
-            .title = (
-                'Leave Credit'
-            );
-            
-            formData
-            .inputs = getInputs();
-
-            formService
-            .init(
-                formData, 
-                gridData, 
-                row, 
-                'leaveCredits',
-                false
-            );
-        }
-        
-        function initializeAdd( formData, gridData, parentRow ){
-            
-            formData
-            .model = {
-                regularStaffID : 
-                    parentRow
-                    .entity
-                    .regularStaffID
-            };
-            
-            formData
-            .isEditing = (
-                false
-            );
-            
-            formData
-            .title = (
-                'Leave Credit'
-            );
-            
-            
-            formData
-            .inputs = getInputs();
-
-            formService
-            .init(
-                formData, 
-                gridData, 
-                null, 
-                'leaveCredits',
-                false
-            );
-        }
-        
-        function create( formData ){
-            return $http
-            .post(
-                '/LeaveCredit/', 
-                formData
-                .model
-            );
-        }
-        
-        function update( formData ){
-            return $http
-            .put(
-                '/LeaveCredit/' + 
-                formData
-                .model
-                .leaveCreditID, 
-                formData
-                .model
-            );
-        }
-        
-        function remove( formData ){
-            return $http
-            .delete(
-                '/LeaveCredit/' + 
-                formData
-                .model
-                .leaveCreditID
-            );   
-        }
-        
-        function getInputs(){
-            return [
-                {
-                    type: 
-                        "number",
-                    name: 
-                        "amount",
-                    label: 
-                        "Credit",
-                    required: 
-                        true
-                }, {
-                    type: 
-                        "date",
-                    name: 
-                        "startDate",
-                    label: 
-                        "Start Date",
-                    required: 
-                        true
-                }, {
-                    type: 
-                        "date",
-                    name: 
-                        "endDate",
-                    label: 
-                        "End Date",
-                    required: 
-                        true,
-                    minDate: 
-                        "startDate"
-                }, {
-                    type: 
-                        "select",
-                    name: 
-                        "leaveCreditType",
-                    label: 
-                        "Type",
-                    items: 
-                        [
-                            "Research",
-                            "Administrative"
-                        ],
-                    path: 
-                        "leaveCreditType",
-                    text:
-                        "Select a Credit Type",
-                    required: 
-                        true
-                }, {
-                    type: 
-                        "textarea",
-                    name: 
-                        "description",
-                    label: 
-                        "Eligibility"
+                    (function(gridOptions) {
+                        gridApi.selection.on.rowSelectionChanged(null, function(row) {
+                            initializeEdit($scope.formData, gridOptions.data, row);
+                        });
+                    })(this);
                 }
-            ];
-        }
+            };
+        },
+        initAddForm: initializeAdd,
+        initEditForm: initializeEdit,
+        create: create,
+        update: update,
+        delete: remove
+    };
+
+    function initializeEdit(formData, gridData, row) {
+        formData.model = (_.cloneDeep(row.entity));
+        formData.isEditing = (true);
+        formData.title = ('Leave Credit');
+        formData.inputs = getInputs();
+
+        formService.init(formData, gridData, row, 'leaveCredits', false);
     }
-);
+
+    function initializeAdd(formData, gridData, parentRow) {
+        formData.model = {
+            regularStaffID: parentRow.entity.regularStaffID
+        };
+        formData.isEditing = (false);
+        formData.title = ('Leave Credit');
+        formData.inputs = getInputs();
+
+        formService.init(formData, gridData, null, 'leaveCredits', false);
+    }
+
+    function create(formData) {
+        return $http.post('/LeaveCredit/', formData.model);
+    }
+
+    function update(formData) {
+        return $http.put('/LeaveCredit/' + formData.model.leaveCreditID, formData.model);
+    }
+
+    function remove(formData) {
+        return $http.delete('/LeaveCredit/' + formData.model.leaveCreditID);
+    }
+
+    function getInputs() {
+        return [{
+            type: "number",
+            name: "amount",
+            label: "Credit",
+            required: true
+        }, {
+            type: "date",
+            name: "startDate",
+            label: "Start Date",
+            required: true
+        }, {
+            type: "date",
+            name: "endDate",
+            label: "End Date",
+            required: true,
+            minDate: "startDate"
+        }, {
+            type: "select",
+            name: "leaveCreditType",
+            label: "Type",
+            items: [
+                "Research",
+                "Administrative"
+            ],
+            path: "leaveCreditType",
+            text: "Select a Credit Type",
+            required: true
+        }, {
+            type: "textarea",
+            name: "description",
+            label: "Eligibility"
+        }];
+    }
+});
--- assets/components/content/leaves/tabs/leaveTabs.js
+++ assets/components/content/leaves/tabs/leaveTabs.js
@@ -1,223 +1,94 @@
-/* global
-    application
-*/
+/* global application */
 
-application
-.service(
-    'leaveTabs', 
-    function(
-        $http, 
-        leaveCredits,
-        leaveDebits,
-        formService
-    ){
-        return {
-            tabs : function( $scope ){
-                return {
-                    credits : {
-                        title: 
-                            'Earned',
-                        gridOptions: 
-                            leaveCredits
-                            .gridOptions( $scope ),
-                        initializeAdd:
-                            leaveCredits
-                            .initAddForm,
-                        initializeEdit:
-                            leaveCredits
-                            .initEditForm
-                    },
-                    debits : {
-                        title: 
-                            'Taken',
-                        gridOptions: 
-                            leaveDebits
-                            .gridOptions( $scope ),
-                        initializeAdd:
-                            leaveDebits
-                            .initAddForm,
-                        initializeEdit:
-                            leaveDebits
-                            .initEditForm
-                    }  
-                };
-            },
-            initializeTabs : function( tabs, row ){
-                var regularStaffID; 
-                
-                regularStaffID= (
-                    row
-                    .entity
-                    .regularStaffID
-                );
-                
-                row
-                .entity
-                .administrativeBalance = 0;
-                
-                row
-                .entity
-                .researchBalance = 0;
-                
-                row
-                .entity
-                .completed = 0;
-                
-                getCredits( 
-                    tabs
-                    .credits,
-                    regularStaffID,
-                    row
-                    .entity
-                );
-                
-                getDebits( 
-                    tabs
-                    .debits,
-                    regularStaffID,
-                    row
-                    .entity
-                );
-                
-            }
-        };
-        
-        function getCredits( creditsTab, regularStaffID, rowEntity ){
-            
-            $http
-            .get(
-                '/regularStaff/getInfo?type=leaveCredits&id=' + regularStaffID
-            )
-            .then( 
-                function( res ) {
-                    res
-                    .data
-                    .map(function( value ){
-                        return Object.
-                        assign(
-                            value,
-                            {
-                                startDate : 
-                                    formService
-                                    .formatDate(
-                                        value
-                                        .startDate
-                                    ),
-                                endDate : 
-                                    formService
-                                    .formatDate(
-                                        value
-                                        .endDate
-                                    )
-                            }   
-                        );    
-                    });
-                    
-                    rowEntity.administrativeBalance += res
-                    .data
-                    .filter(function( value ){
-                        return value.leaveCreditType === 'Administrative';
-                    })
-                    .map(function( value ){
-                        return value.amount;
-                    })
-                    .reduce(function( previous, current){
-                        return previous + current;
-                    },0);
-                    
-                    rowEntity.researchBalance += res
-                    .data
-                    .filter(function( value ){
-                        return value.leaveCreditType === 'Research';
-                    })
-                    .map(function( value ){
-                        return value.amount;
-                    })
-                    .reduce(function( previous, current){
-                        return previous + current;
-                    },0);
-                    
-                    rowEntity
-                    .completed ++;
-                    
-                    creditsTab
-                    .gridOptions
-                    .data = (
-                        res
-                        .data
-                    );
+application.service('leaveTabs', function($http, leaveCredits, leaveDebits, formService) {
+    return {
+        tabs: function($scope) {
+            return {
+                credits: {
+                    title: 'Earned',
+                    gridOptions: leaveCredits.gridOptions($scope),
+                    initializeAdd: leaveCredits.initAddForm,
+                    initializeEdit: leaveCredits.initEditForm
+                },
+                debits: {
+                    title: 'Taken',
+                    gridOptions: leaveDebits.gridOptions($scope),
+                    initializeAdd: leaveDebits.initAddForm,
+                    initializeEdit: leaveDebits.initEditForm
                 }
-            );
-            
-        } 
-        
-        function getDebits( debitsTab , regularStaffID, rowEntity ){
-            $http
-            .get(
-                '/regularStaff/getInfo?type=leaveDebits&id=' + regularStaffID
-            )
-            .then( 
-                function( res ) {
-                    
-                    res
-                    .data
-                    .map(function( value ){
-                        return Object.
-                        assign(
-                            value,
-                            {
-                                startDate : 
-                                    formService
-                                    .formatDate(
-                                        value
-                                        .startDate
-                                    ),
-                                endDate : 
-                                    formService
-                                    .formatDate(
-                                        value
-                                        .endDate
-                                    )
-                            }   
-                        );    
-                    });
-                    
-                    rowEntity.administrativeBalance -= res
-                    .data
-                    .filter(function( value ){
-                        return value.leaveDebitType === 'Administrative';
-                    })
-                    .map(function( value ){
-                        return value.amount;
-                    })
-                    .reduce(function( previous, current){
-                        return previous + current;
-                    },0);
-                    
-                    rowEntity.researchBalance -= res
-                    .data
-                    .filter(function( value ){
-                        return value.leaveDebitType === 'Research';
-                    })
-                    .map(function( value ){
-                        return value.amount;
-                    })
-                    .reduce(function( previous, current){
-                        return previous + current;
-                    },0);
-                    
-                    rowEntity
-                    .completed ++;
-                    
-                    debitsTab
-                    .gridOptions
-                    .data = (
-                        res
-                        .data
-                    );
-                }
-            );
+            };
+        },
+        initializeTabs: function(tabs, row) {
+            var regularStaffID;
+
+            regularStaffID = (row.entity.regularStaffID);
+            row.entity.administrativeBalance = 0;
+            row.entity.researchBalance = 0;
+            row.entity.completed = 0;
+
+            getCredits(tabs.credits, regularStaffID, row.entity);
+            getDebits(tabs.debits, regularStaffID, row.entity);
         }
-    }
-);
+    };
 
+    function getCredits(creditsTab, regularStaffID, rowEntity) {
+        $http.get('/regularStaff/getInfo?type=leaveCredits&id=' + regularStaffID)
+            .then(function(res) {
+                res.data.map(function(value) {
+                    return Object.assign(value, {
+                        startDate: formService.formatDate(value.startDate),
+                        endDate: formService.formatDate(value.endDate)
+                    });
+                });
+
+                rowEntity.administrativeBalance += res.data.filter(function(value) {
+                    return value.leaveCreditType === 'Administrative';
+                }).map(function(value) {
+                    return value.amount;
+                }).reduce(function(previous, current) {
+                    return previous + current;
+                }, 0);
+
+                rowEntity.researchBalance += res.data.filter(function(value) {
+                    return value.leaveCreditType === 'Research';
+                }).map(function(value) {
+                    return value.amount;
+                }).reduce(function(previous, current) {
+                    return previous + current;
+                }, 0);
+
+                rowEntity.completed++;
+                creditsTab.gridOptions.data = (res.data);
+            });
+    }
+
+    function getDebits(debitsTab, regularStaffID, rowEntity) {
+        $http.get('/regularStaff/getInfo?type=leaveDebits&id=' + regularStaffID).then(function(res) {
+            res.data.map(function(value) {
+                return Object.assign(value, {
+                    startDate: formService.formatDate(value.startDate),
+                    endDate: formService.formatDate(value.endDate)
+                });
+            });
+
+            rowEntity.administrativeBalance -= res.data.filter(function(value) {
+                return value.leaveDebitType === 'Administrative';
+            }).map(function(value) {
+                return value.amount;
+            }).reduce(function(previous, current) {
+                return previous + current;
+            }, 0);
+
+            rowEntity.researchBalance -= res.data.filter(function(value) {
+                return value.leaveDebitType === 'Research';
+            }).map(function(value) {
+                return value.amount;
+            }).reduce(function(previous, current) {
+                return previous + current;
+            }, 0);
+
+            rowEntity.completed++;
+            debitsTab.gridOptions.data = (res.data);
+        });
+    }
+});
